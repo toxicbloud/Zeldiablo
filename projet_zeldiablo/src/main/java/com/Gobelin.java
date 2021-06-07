3eb8ed878cd9565777ea17ca0654187255def15c
Celian BAUER <celian.bauer7@etu.univ-lorraine.fr>
2021-06-07 17:33:18 +0200

    javadoc monstre + gobelin

--- projet_zeldiablo/src/main/java/com/Gobelin.java
+++ projet_zeldiablo/src/main/java/com/Gobelin.java
@@ -1,11 +1,26 @@
 package com;
 
+/**
+ * classe qui représente un gobelin
+ */
 public class Gobelin extends Monstre {
     
+    /**
+     * constructeur de gobelin
+     * @param n nom du gobelin
+     * @param vie point de Vie du gobelin
+     * @param v position
+     * @param l Labyrinthe du gobelin
+     * @param d dégât du gobelin
+     * @param desc description du gobelin
+     */
     public Gobelin(int d, int vie, Vec2 v, Labyrinthe l) {
         super("Gobelin", v, vie, d, "Le Gobelin est un monstre faible mais rapide", l);
     }
 
+    /**
+     * methode qui permet à un gobelin de se déplacer
+     */
     public void deplacer() {
         int rand = (int)(Math.random() * 4);
         switch(rand) {
--- projet_zeldiablo/src/main/java/com/Monstre.java
+++ projet_zeldiablo/src/main/java/com/Monstre.java
@@ -1,10 +1,22 @@
 package com;
-
+/**
+ * classe qui représente les différents monstres du jeu
+ */
 public abstract class Monstre extends Entite {
     
     private int degat;
     private String description;
 
+
+    /**
+     * constructeur de monstre
+     * @param n nom de l'entite
+     * @param vie point de Vie de l'entité
+     * @param v position
+     * @param l Labyrinthe de l Entite
+     * @param d dégât du monstre
+     * @param desc description du monstre
+     */
     public Monstre(String n, Vec2 v, int vie, int d, String desc, Labyrinthe l) {
         super(n, v, vie, l);
         this.degat = d;
@@ -12,20 +24,37 @@ public abstract class Monstre extends Entite {
 
     }
 
+    /**
+     * methode qui permet à un monstre de soigner une entité
+     * @param soin soin que le monstre va administrer à l'entité
+     * @param soignee entitée qui va etre soignée
+     */
     public void soigner(int soin, Entite soignee) {
         if (!etreMort()) {
             soignee.gagnerPV(soin);
         }
     }
 
+    /**
+     * methode qui retourne les degats du monstre
+     * @return int degat du monstre
+     */
     public int getDegat() {
         return this.degat;
     }
 
+    /**
+     * methode qui permet de changer les degats du monstre
+     * @param d nouveau degat du monstre
+     */
     public void setDegat(int d) {
         this.degat = d;
     }
 
+    /**
+     * methode qui retourne la description du monstre
+     * @return String description du monstre
+     */
     public String getDescription() {
         return this.description;
     }
