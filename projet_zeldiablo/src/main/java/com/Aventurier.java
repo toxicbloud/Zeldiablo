2e186190f82eb1d3d80a7a731490e19b447fb7cd
toxicbloud <fanta.r55000@gmail.com>
2021-06-07 16:29:57 +0200

    Update Aventurier.java

--- projet_zeldiablo/src/main/java/com/Aventurier.java
+++ projet_zeldiablo/src/main/java/com/Aventurier.java
@@ -3,9 +3,6 @@ import java.awt.Graphics;
 /**
  * @author Antonin
  */
-import java.awt.image.BufferedImage;
-
-import moteurJeu.DessinJeu;
 
 public class Aventurier extends Entite{
 
@@ -13,15 +10,7 @@ public class Aventurier extends Entite{
      * constructeur aventurier avec un nom
      * @param nom nom de l aventurier
      */
-    public Aventurier(String nom,Vec2 v,int pv){
-        super(nom,v,pv);
+    public Aventurier(String nom,Vec2 v,int pv,Labyrinthe l){
+        super(nom,v,pv,l);
     }
-
-    @Override
-    public void dessiner(BufferedImage image){
-        Graphics g = image.getGraphics();
-        g.fillOval(this.getPos().x, this.getPos().y, 10, 10);
-
-    }
-
 }
