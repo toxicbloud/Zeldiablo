a03640a9aaade55f5e8d2a309f988d81e4ec9955
Celian BAUER <celian.bauer7@etu.univ-lorraine.fr>
2021-06-07 15:27:25 +0200

    modification diagramme classe

---
+++ projet_zeldiablo/src/main/java/com/Monstre.java
@@ -0,0 +1,12 @@
+package com;
+
+public abstract class Monstre extends Entite {
+    
+    private int degat;
+    private String description;
+
+    public Monstre(String nom) {
+        super(nom);
+
+    }
+}
