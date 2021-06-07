70e29849b0c94d91ff6ed792aee132705531f39b
Celian BAUER <celian.bauer7@etu.univ-lorraine.fr>
2021-06-07 16:18:12 +0200

    changement classe Gobelin + classe Monstre

--- projet_zeldiablo/src/main/java/com/Gobelin.java
+++ projet_zeldiablo/src/main/java/com/Gobelin.java
@@ -1,8 +1,8 @@
 package com;
 
-public class Gobelin {
+public class Gobelin extends Monstre {
     
-    public Gobelin(int d, int vie) {
-        super("Gobelin", vie, d, "Le Gobelin est un monstre faible mais rapide");
+    public Gobelin(int d, int vie, int x, int y) {
+        super("Gobelin", new Vec2(x, y), vie, d, "Le Gobelin est un monstre faible mais rapide");
     }
 }
