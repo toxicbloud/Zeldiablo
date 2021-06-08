54c0c7bfd72468d6a3859af545323893a71bcfa3
Talluan <tmclaudel@gmail.com>
2021-06-08 17:49:07 +0200

    documentation arme

--- projet_zeldiablo/src/main/java/com/Arme.java
+++ projet_zeldiablo/src/main/java/com/Arme.java
@@ -11,6 +11,10 @@ public abstract class Arme {
     private int range ; 
 
 
+    /**
+     * Constructeur de arme
+     * @param de degats
+     */
     public Arme(int de){
         if (de<=0) {
             this.degats=1;
@@ -20,40 +24,84 @@ public abstract class Arme {
         }
     }
 
+    /**
+     * Constructeur de arme
+     * @param de degats
+     * @param r range
+     */
     public Arme(int de, int r) {
         this.degats = de;
         this.range = r;
     }
 
+    /**
+     * attaque une entité
+     * @param e1 entité à attaquer
+     */
     public void attaquer(Entite e1){
         e1.perdrePV(degats);
     }
 
+    /**
+     * donne les degats
+     * @return degats
+     */
     public int getDegats() {
 		return this.degats;
 	}
 
+    /**
+     * initialise les degats
+     * @param degats
+     */
 	public void setDegats(int degats) {
 		this.degats = degats;
 	}
 
+    /**
+     * donne la vitesse
+     * @return vitesse
+     */
 	public int getVitesse() {
 		return this.vitesse;
 	}
 
+    /**
+     * initialise la vitesse
+     * @param vitesse
+     */
 	public void setVitesse(int vitesse) {
 		this.vitesse = vitesse;
 	}
 
+    /**
+     * donne le possesseur
+     * @return l'entité
+     */
     public Entite getPossesseur() {
         return possesseur;
     }
+
+    /**
+     * initialise le possesseur
+     * @param possesseur
+     */
     public void setPossesseur(Entite possesseur) {
         this.possesseur = possesseur;
     }
+
+    /**
+     * donne la range
+     * @return range
+     */
     public int getRange() {
         return range;
     }
+
+    /**
+     * initialise la range
+     * @param range
+     */
     public void setRange(int range) {
         this.range = range;
     }
--- projet_zeldiablo/src/main/java/com/Couteau.java
+++ projet_zeldiablo/src/main/java/com/Couteau.java
@@ -2,6 +2,11 @@ package com;
 
 public class Couteau extends Arme_COC {
     
+    /**
+     * Construit un couteau
+     * @param d degats
+     * @param e energie que cela coute
+     */
     public Couteau(int d, int e) {
         super(d, e);
     }
