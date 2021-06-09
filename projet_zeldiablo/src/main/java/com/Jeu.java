5260a8ecba6100e0257edee0e60d9670bb06d689
FurWaz <fur.waz06@gmail.com>
2021-06-09 10:09:01 +0200

    Reglement d'un bug

--- projet_zeldiablo/src/main/java/com/Jeu.java
+++ projet_zeldiablo/src/main/java/com/Jeu.java
@@ -28,8 +28,8 @@ public class Jeu implements moteurJeu.Jeu {
             labyrinthes[i] = new Labyrinthe();
         
         this.ennemis = new ArrayList<Entite>();
+        this.ennemis.add(new Gobelin(1, this.getCurrentLabyrinthe().getEntree().times(Labyrinthe.TILE_SIZE), getCurrentLabyrinthe()));
         this.joueur = new Aventurier(n, this.getCurrentLabyrinthe().getEntree().times(Labyrinthe.TILE_SIZE), this.getCurrentLabyrinthe(), this);
-        this.ennemis.add(new Gobelin(1, this.getCurrentLabyrinthe().getEntree(), getCurrentLabyrinthe()));
         this.cam = new Camera(this.joueur);
     }
 
