5de842ab1e2cab3dd3851b2f991b9746580231b1
Talluan <tmclaudel@gmail.com>
2021-06-09 09:31:56 +0200

    modif jeuµ.java

--- projet_zeldiablo/src/main/java/com/Jeu.java
+++ projet_zeldiablo/src/main/java/com/Jeu.java
@@ -30,18 +30,16 @@ public class Jeu implements moteurJeu.Jeu {
     }
 
     /** 
-     * Constructeur vide de Jeu 
-     * @param n nom de l aventurier
+     * Constructeur vide de Jeu pour les tests
      */
-    public Jeu(String n, Entite ennemi) {
+    public Jeu() {
         Textures.chargerTextures();
         this.ennemis = new ArrayList<Entite>();
         this.carte = new Labyrinthe();
-        this.joueur = new Aventurier(n, this.carte.getEntree().times(Labyrinthe.TILE_SIZE), this.carte, this);
+        this.joueur = new Aventurier("testeur", this.carte.getEntree().times(Labyrinthe.TILE_SIZE), this.carte, this);
         this.cam = new Camera(this.joueur);
-        this.ennemis.add(new Gobelin(5, new Vec2(2, 2), this.carte));
         this.ennemis.add(new Gobelin(5, carte.getEntree().times(Labyrinthe.TILE_SIZE), this.carte));
-        this.ennemis.add(ennemi);
+        this.ennemis.add(new Gobelin(5, new Vec2(2, 2), this.carte));
     }
 
     /** Methode evoluer utilisee par le moteur de jeu */
