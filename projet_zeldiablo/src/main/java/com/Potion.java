1475ce2b013c906cba90cf4ba2dd54f7955fba25
toxicbloud <fanta.r55000@gmail.com>
2021-06-11 14:11:18 +0200

    Create Potion.java

---
+++ projet_zeldiablo/src/main/java/com/Potion.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Potion extends Case{
+ public Potion(){
+     super(true,Textures.tex_hpPotion);
+ }
+    @Override
+    public void action(Jeu j) {
+        if(j.getJoueur().getPV()>99){return;}
+        j.getJoueur().gagnerPV(10);
+        // j.getCarte().getCaseAtVec2(j.getJoueur().getPos());
+        Case  lab[][]= j.getCarte().getCases();
+        Vec2 newPos = j.getJoueur().getPos().plus(new Vec2(Labyrinthe.TILE_SIZE/2, Labyrinthe.TILE_SIZE/2)).div(Labyrinthe.TILE_SIZE);
+        lab[newPos.x][newPos.y]=new Chemin();
+    }
+    
+}
